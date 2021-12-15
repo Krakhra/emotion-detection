0b1d8003c7e52b2b49aea9ffb41d9ac395f73662
krakhra <kiratrakhra@gmail.com>
2021-12-14 22:43:38 -0500

    reset file

--- script.py
+++ script.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import neattext.functions as nfx
+from textblob import TextBlob
+
+def get_sentiment(text):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment
+    return sentiment
+
+print(get_sentiment("I love coding"))
