2fe4d47045ef00e2f0b0516952877617b19b8a90
krakhra <kiratrakhra@gmail.com>
2021-12-29 12:41:10 -0500

    created flask server

--- server.py
+++ server.py
@@ -1,4 +1,16 @@
 import pickle
+from flask import Flask 
 
 with open('model_pkl', 'rb') as f:
     data = pickle.load(f)
+
+# flask setup
+app = Flask(__name__)
+
+@app.route("/getValue/<text>")
+def getVal(text):
+    return data.predict([text])[0]
+
+if __name__ == "__main__":
+    app.run()
+
